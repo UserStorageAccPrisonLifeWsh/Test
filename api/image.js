1b32ee01fff0c1888e3e757f5293af130029d23a
UserStorageAccPrisonLifeWsh <sxirbes@gmail.com>
2025-01-14 19:47:13 +0100

    Update image.js

--- api/image.js
+++ api/image.js
@@ -1,19 +1,1 @@
-// api/image.js
-export default function handler(req, res) {
-    if (req.method === 'POST') {
-        // Holen der URL aus dem Body der Anfrage
-        const { url } = req.body;
-        console.log('Empfangene URL:', url);
 
-        // Hier kannst du weitere Logik hinzufügen, um mit der URL zu arbeiten
-
-        // Antwort zurückgeben
-        res.status(200).json({
-            message: 'URL empfangen und verarbeitet',
-            receivedUrl: url
-        });
-    } else {
-        // Fehlerantwort für andere Methoden
-        res.status(405).json({ message: 'Method Not Allowed' });
-    }
-}
